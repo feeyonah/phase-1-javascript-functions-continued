d8c8b540d0435141345af7115f5215618b57a36a
fiona <omburafiona@gmail.com>
2023-08-07 13:35:35 +0300

    functions continued lab completed

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-function saturdayFun(activity = "rollerskate") {
-    return `This Saturday, I want to go ${activity}!`;
-  }
-  
-  function mondayWork(duty = "go to the office") {
-    return `This Monday, I will ${duty}.`;
-  }
-  
-  function wrapAdjective(symbol) {
-    return function (adjective) {
-      return `You are ${symbol}${adjective}${symbol}!`;
-    };
-  }
-   
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(duty = "go to the office") {
+  return `This Monday, I will ${duty}.`;
+}
+
+function wrapAdjective(symbol) {
+  return function (adjective) {
+    return `You are ${symbol}${adjective}${symbol}!`;
+  };
+}
